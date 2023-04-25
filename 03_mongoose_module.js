62e19139b28236cfc8cbe1b86f7eabbba0c7258e
Eugene2077 <aosk@mail.com>
2023-04-25 05:42:23 -0400

    init2

--- 03_mongoose_module.js
+++ 03_mongoose_module.js
@@ -1,4 +1,7 @@
 
 // mongoose install : npm i mongoose
 
-require
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
